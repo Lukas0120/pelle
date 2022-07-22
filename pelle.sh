7bed77917f769ccf0b5ebadbf82fb4ea5f798c2a
Lukas0120 <Lukas.swe@hotmail.com>
2022-07-22 09:52:28 +0200

    dawd
    awd

--- pelle.sh
+++ pelle.sh
@@ -3,43 +3,28 @@
 sudo apt update
 sudo apt upgrade -y
 
-wget https://apt.llvm.org/llvm.sh
-chmod +x llvm.sh
-sudo ./llvm.sh 15 all
-
-#tar xvf pelle.tar.xz
-
-sudo apt install -y wget git curl zram-config systemd-oomd zstd micro lz4 zsh zsh-antigen python-is-python3 lolcat neofetch build-essential llvm lld clang bc bison ccache flex binutils-dev ca-certificates cmake file libelf-dev libssl-dev make ninja-build python3-dev texinfo u-boot-tools xz-utils xfsprogs xfsdump zlib1g-dev
-
-mv -vf ~/pelle/* ~/
-
+tar xvf pelle.tar.xz
 cd ~
-
-mv -v ~/neofetch ~/.config/
-mv -vf ~/fonts ~/.local/share/
-
-cd autojump
-./install.py
-cd ~
-
+sudo apt install -y openbox openbox-menu menu obconf xorg lightdm lightdm-gtk-greeter lxappearance
+sudo apt install -y wget git curl xfce4-terminal thunar tint2 xfce4-settings zstd micro lz4 zsh zsh-antigen python-is-python3 cmake file make ninja-build python3-dev xfsprogs xfsdump
+mkdir -p /home/lulle/.local/share
+cp -rf /home/lulle/pelle/fonts /home/lulle/.local/share/
 fc-cache -fv
+cp -rf /home/lulle/pelle/home/* ~~
+cp -rf /home/lulle/pelle/home/. ~~
+mkdir -p ~/.config/openbox
+cp -a /etc/xdg/openbox/ ~/.config/
+wget https://download.puffinbrowser.com/repo/pool/stable/arm64/puffin-internet-terminal-demo_8.2.4.705_arm64.deb
+sudo apt install /home/lulle/puffin-internet-terminal-demo_8.2.4.705_arm64.deb
+sudo apt install -y mesa-vulkan-drivers libvulkan-dev libvulkan1 vulkan-tools
 
-wget https://lullemannen.com/clang.tar.lz4
-lz4 -d clang.tar.lz4
-tar xvf clang.tar
 
-wget https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-6.9.1-Linux-x64.deb?_ga=2.69054488.801916066.1649574969-77873647.1649574969
-sudo dpkg -i VNC*
-sudo vnclicense -add HVBA2-R272B-7HT29-T9JHL-F8KM3
-sudo vncserver-x11 -service -joinCloud eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJwbGF0Zm9ybS1lbnRlcnByaXNlLXBvcnRhbDp5bWJVQXpiTk1tMVBlNUhFc3h2Iiwic3ViIjoiM0g5T1VzbkNXenU4NFY3Q2h1UyIsImF1ZCI6ImNyZWF0ZS1zZXJ2ZXIiLCJpZCI6IkVBRUZ2aU56dFNoemJIeUtla3JDIiwiaWF0IjoxNjQ3MTcxODIzfQ.O3zgB8r8k4qOWVd2EqMx4Zj2t72ugB0dZnMFkxW0AzM
-sudo systemctl start vncserver-x11-serviced.service
-sudo systemctl enable vncserver-x11-serviced.service
 
-sudo systemctl enable systemd-oomd
 
-mkdir -p ~/.config/dpkg
-mkdir -p ~/repos
 
-cd repos
-git clone https://github.com/frogging-family/linux-tkg.git
-git clone https://github.com/pyavitz/rpi-img-builder.git
+
+
+
+
+
+
